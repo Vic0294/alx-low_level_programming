fa9844a635eae9ce4d02fbfa888894fe037d3067
Vic0294 <iwuegbuchimaobi@gmail.com>
2022-09-12 02:27:28 -0700

    Hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Print a string
+*
+* Return: ALways 0 (Successs)
+*/
+
+int main(void)
+
+{
+	printf("%s", "with proper, but the outcome is a piece of art,\n");
+	return (0);
+
+}
